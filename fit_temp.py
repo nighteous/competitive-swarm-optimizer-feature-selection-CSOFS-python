be8d1436549f4def72e40ee5e8d1f59766e98e18
Varenyam Bhardwaj <varenyambhardwaj123@gmail.com>
2022-11-12 17:37:15 +0530

    Smile_subsample_segments loop working

--- fit_temp.py
+++ fit_temp.py
@@ -13,5 +13,16 @@ def func(data, inp):
     k = 10
     group = data[:, -1]
     classes = np.unique(group)
-    print(classes)
+    data_group = []
+    smile_subsample_segments = []
+    
+    for i in range(len(classes)):
 
+        sa = []
+        sa = np.array([data[ind] for ind in range(len(group)) if group[ind] == classes[i]])
+        
+        number_of_smile_samples, _ = sa.shape
+        smile_temp = np.round(np.linspace(1, number_of_smile_samples, k + 1))
+
+        data_group.append(sa)
+        smile_subsample_segments.append(smile_temp)
