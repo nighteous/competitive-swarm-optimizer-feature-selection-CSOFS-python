0480edc2f34242e73638e9bfd544e6609895dd66
Varenyam Bhardwaj <varenyambhardwaj123@gmail.com>
2022-11-12 17:05:56 +0530

    Defined sigmoid function

--- main.py
+++ main.py
@@ -58,6 +58,15 @@ class CSOFS:
 
 
 
+    def sigmoid(self, x: np.ndarray, parms: List[int]):
+        """
+        This function computes fuzzy membership values using a sigmoidal membership function
+        """
+
+        f = 1 / (1 + np.exp(-parms[0] * (x - parms[1])))
+
+        return f
+
 
 
 def main():
