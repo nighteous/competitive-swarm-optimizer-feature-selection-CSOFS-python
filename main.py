1949881308beff0ddd82d4ecd5cc346a29133f7c
Varenyam Bhardwaj <varenyambhardwaj123@gmail.com>
2022-11-26 16:41:32 +0530

    Previously we were taking a row, we needed the first column of losers

--- main.py
+++ main.py
@@ -2,17 +2,19 @@
 #  Implementation of a competitive swarm optimizer (CSO) for feature selction in python
 #  See the details of CSO in the following paper
 #  Cite as:S. Gu, R. Cheng, Y. Jin, Feature Selection for High-Dimensional Classification using a Competitive Swarm Optimizer, Soft Computing. 22 (2018) 811-822.
-#  The source code CSO is implemented by Yang Xuesen 
-#  If you have any questions about the code, please contact: 
+#  The source code CSO is implemented by Yang Xuesen
+#  If you have any questions about the code, please contact:
 #  Python Implementation:
 #  Varenyam at varenyam17@gmail.com
 #
-#  Matlab Implementation: 
+#  Matlab Implementation:
 #  Yang Xuesen at 1348825332@qq.com
 #  Institution: Shenzhen University
 #########################################################################################
 
 from typing import List
+
+from tqdm import tqdm
 
 from scipy.io import loadmat
 import numpy as np
@@ -20,11 +22,11 @@ import numpy.matlib as matlib
 
 from fit_temp import func
 
-class CSOFS: 
+class CSOFS:
     def __init__(self):
         sonar = loadmat("sonar.mat")
         self.X, self.data, self.gnd = sonar['X'], sonar['data'], sonar['gnd']
-        # n: dimensionality    
+        # n: dimensionality
         self.n = np.size(self.data, 1) - 1
         # maxfe: maximal number of fitness evalutations
         self.maxfe = 100
@@ -37,10 +39,10 @@ class CSOFS:
         self.phi = 0.1
         self.results = np.zeros((1, self.runnum))
 
-        
+
 
     def run(self):
-        for i in range(0, self.runnum):
+        for i in tqdm(range(0, self.runnum)):
             XRRmin = matlib.repmat(self.lu[0, :], self.m, 1)
             XRRmax = matlib.repmat(self.lu[1, :], self.m, 1)
 
@@ -51,14 +53,14 @@ class CSOFS:
 
             randNum = np.random.rand(self.m, self.n)
             change_position = (pop > randNum)
-            
+
             bi_position[change_position] = 1
 
             feature = []
             for i in range(0, self.m):
                 feature = [idx for idx in range(len(bi_position[i])) if bi_position[i][idx] == 1]
                 self.fitness[i] = func(self.data, feature)
-                
+
             v = np.zeros((self.m, self.n))
             bestever = 1e200
 
@@ -87,29 +89,31 @@ class CSOFS:
                 p[losers] = p[losers] + v[losers]
 
                 for _ in range(0, int(np.ceil(self.m/2))):
-                    x = np.maximum(p[losers[_], :], self.lu[0, :])
-                    y = np.minimum(p[losers[_], :], self.lu[1, :])
-                    p[losers[_], :] = x
-                    p[losers[_], :] = y
+
+                    x = np.maximum(p[losers[:,0][_ - 1]], self.lu[0])
+                    y = np.minimum(p[losers[:,0][_ - 1]], self.lu[1])
+
+                    row = losers[:,0][_ - 1]
+                    p[row] = x
+                    p[row] = y
+
+                        
 
                 # fitness evalutation
-                n_losers = len(losers)
+                n_losers = len(losers[:,0])
                 for id in range(n_losers):
-                    pop[losers[id]] = self.sigmoid(p[losers[id]], [1, 0])
-                    randnu = np.random.rand(1, self.n)
-                    change_pos = (pop[losers[id]] > randnu)
+                    pop[losers[:,0][id - 1]] = self.sigmoid(p[losers[:,0][id - 1]], [1, 0])
+                    randnu = np.random.rand(0, self.n)
+                    change_pos = (pop[losers[:,0][id - 1]] > randnu)
                     indices = np.where(change_pos == 1)
 
 
-                    self.fitness[losers[id]] = func(self.data, feature)
-                    
+                    self.fitness[losers[:,0][id - 1]] = func(self.data, feature)
+
                 bestever = np.minimum(bestever, np.min(self.fitness))
-                print(f"Round {gen}: Best Fitness: {bestever:.10} Current Fitness: {np.min(self.fitness):.10}")
+                print(f"Round {gen}: Best Fitness: {bestever} Current Fitness: {np.min(self.fitness)}")
 
                 gen += 1
-
-
-
 
 
     def sigmoid(self, x: np.ndarray, parms: List[int]):
